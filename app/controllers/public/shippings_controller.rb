314b8709fe54a88025012109e0537bf52314b221
vagrant <vagrant@localhost.localdomain>
2020-09-02 08:45:53 +0000

    shippingsの途中

--- app/controllers/public/shippings_controller.rb
+++ app/controllers/public/shippings_controller.rb
@@ -1,2 +1,35 @@
 class Public::ShippingsController < ApplicationController
+
+    #配送先一覧
+  def index
+    	@shippings= Shipping.all
+        @shipping = Shipping.new
+  end
+   #配送先新規保存
+  def create
+  	@shipping = Shipping.new(shipping_params)
+  	@shipping.save
+  	redirect_to shipping_path(@shipping.id)
+  end
+   #配送先編集
+  def edit
+  	@shipping = Shipping.find(params[:id])
+  end
+   #編集内容保存
+  def update
+  	@shipping = Shippinng.find(params[:id])
+  	@shipping.update(shipping_params)
+  	redirect_to shipping_path(@shipping.id)
+  end
+    #配送先削除
+  def destroy
+  	@shipping = Shipping.find(params[:id])
+  	@shipping.destroy
+  	redirect_to shipping_path(@deliver.id)
+  end
+
+ private
+    def shippings_params
+        params.require(:shipping).permit(:postal_code, :address,)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_031613) do
+ActiveRecord::Schema.define(version: 2020_09_02_052419) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 2020_09_02_031613) do
     t.integer "total_price"
     t.integer "payment", default: 0
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shippings", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "post_code"
+    t.string "address"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
