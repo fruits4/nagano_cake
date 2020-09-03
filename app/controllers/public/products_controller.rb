bd8727b604bbdca2d7ba879a5f6e6de3b00904d3
nelml <konpaku.youmi@gmail.com>
2020-09-03 15:01:33 +0900

    [update]public toppage,write introduction

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,13 +1,15 @@
 class Public::ProductsController < ApplicationController
 
 	def index
+		@genres = Genre.all
 	end
 
 	def show
 	end
 
 	def top
+		# @genres = Genre.all
 		@genres = Genre.all
-		@products = Product.all
+		@products = Product.where.not(status: "false").shuffle.take(4)
 	end
 end
