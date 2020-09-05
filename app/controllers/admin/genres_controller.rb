da3275cdb25d38e6647daf17a43f0bc8af10e3f2
nelml <konpaku.youmi@gmail.com>
2020-09-05 13:13:34 +0900

    debug genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,8 +5,8 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-  	@genre = genre.new(genre_params)
-  	genre.save
+  	@genre = Genre.new(genre_params)
+  	@genre.save
   	redirect_to admin_genres_path
   end
 
