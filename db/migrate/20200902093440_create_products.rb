1434f4109b2a9129d6821c1ef48a60ca6f70815e
vagrant <vagrant@localhost.localdomain>
2020-09-05 05:59:31 +0000

    productsのäの修正

--- db/migrate/20200902093440_create_products.rb
+++ db/migrate/20200902093440_create_products.rb
@@ -2,11 +2,11 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.integer :genre_id
-      t.string :name, null:false
-      t.string :image_id, null:false
-      t.text :explain, null:false
-      t.boolean :status, default: true, null: false
-      t.integer :price, null:false
+      t.string :name
+      t.string :image_id
+      t.text :explain
+      t.boolean :status, default: true
+      t.integer :price
 
       t.timestamps
     end
