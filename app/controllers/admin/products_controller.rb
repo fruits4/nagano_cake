eba35d2edf5377e6ac655998fa26a3dcc8975c48
vagrant <vagrant@localhost.localdomain>
2020-09-07 06:12:18 +0000

    customer:80%

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -19,8 +19,6 @@ class Admin::ProductsController < ApplicationController
 
 	def create
 		@product = Product.new(product_params)
-		
-		
 		if @product.save
 			redirect_to admin_product_path(@product)
 		else
@@ -30,11 +28,8 @@ class Admin::ProductsController < ApplicationController
 
 	def update
 		@product = Product.find(params[:id])
-		if @product.update(product_params)
-			redirect_to admin_product_path(@product)
-		else
-			render "edit"
-		end
+		@product.update(product_params)
+		redirect_to admin_products_path(@product)
 	end
 
 private
