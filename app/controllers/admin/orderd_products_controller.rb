12408a3dfac48f601466df51d675797f3bbb219c
Chinami Kitagawa <cnm25.k@gmail.com>
2020-09-08 06:05:03 +0000

    [update]admin/orderd_product_チェッãstatus更新

--- app/controllers/admin/orderd_products_controller.rb
+++ app/controllers/admin/orderd_products_controller.rb
@@ -1,6 +1,16 @@
 class Admin::OrderdProductsController < ApplicationController
 
 	def update
+		@order = Order.find(params[:id])
+		@orderd_product = OrderdProduct.find(params[:id])
+		@orderd_product.update(orderd_products_params)
+		redirect_to admin_order_path(@order.id)
 	end
-	
+
+	private
+	def orderd_products_params
+		params.require(:orderd_product).permit(:status)
+	end
+
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,4 +9,25 @@ class Admin::OrdersController < ApplicationController
 		@orderd_products = @order.orderd_products
 	end
 
+	def update
+		@order = Order.find(params[:id])
+		@order.update(orders_params)
+		@orderd_products = @order.orderd_products.all
+		redirect_to admin_order_path(@order.id)
+		#if @order.status == "入金確認"
+		#	@orderd_products.each do |orderd_product|
+		#		orderd_product.update(status: :製作待ち)
+		#	end
+		#else
+		#	render :show
+		#end
+	end
+
+	private
+	def orders_params
+		params.require(:order).permit(:status)
+	end
+
+
+
 end
